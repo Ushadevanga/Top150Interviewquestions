89c07b93def82d0152b95c24239303365d666fde
Devanga Ambe Usha <144150154+Ushadevanga@users.noreply.github.com>
2024-05-03 12:49:06 +0530

    Update Day-31 Add Two Numbers.java

--- Day-31 Add Two Numbers.java
+++ Day-31 Add Two Numbers.java
@@ -1,27 +1,28 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != null || l2 != null || carry != 0) {
-            int digit1 = (l1 != null) ? l1.val : 0;
-            int digit2 = (l2 != null) ? l2.val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode newNode = new ListNode(digit);
-            tail.next = newNode;
-            tail = tail.next;
-
-            l1 = (l1 != null) ? l1.next : null;
-            l2 = (l2 != null) ? l2.next : null;
+        ListNode dummyhead=new ListNode();
+        ListNode current = dummyhead;
+        int carry=0;
+        while(l1!=null||l2!=null||carry!=0){
+            int val1 =0;
+            int val2=0;
+            if(l1!=null){
+                val1 = l1.val;
+            }
+            if(l2!=null){
+                val2=l2.val;
+            }
+            int sum = val1+val2+carry;
+            carry=sum/10;
+            current.next = new ListNode(sum%10);
+            current = current.next;
+            if(l1!=null){
+                l1=l1.next;
+            }
+            if(l2!=null){
+                l2=l2.next;
+            }
         }
-
-        ListNode result = dummyHead.next;
-        dummyHead.next = null;
-        return result;
+        return dummyhead.next;
     }
 }
